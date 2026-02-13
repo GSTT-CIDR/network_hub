280b88cdb47fbb13eb6d2523ad5094f7321ac634
dan-ward-bio <dan_ward93@yahoo.com>
2026-02-13 11:05:58 +0000

    js rollback

--- site/assets/javascripts/version-label.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var selector = document.getElementById("version-selector");
-    if (selector) {
-        var label = document.createElement("span");
-        label.className = "version-label";
-        label.textContent = "Select version";
-        selector.parentNode.insertBefore(label, selector);
-    }
-});
